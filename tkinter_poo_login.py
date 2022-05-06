cda90ab4c950aa35ec8ac4f7140d9df77b86e557
luiszadkiel <104939073+luiszadkiel@users.noreply.github.com>
2022-05-05 23:30:09 -0400

    Add files via upload

---
+++ tkinter_poo_login.py
@@ -0,0 +1,45 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+
+class LoginVentana(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        # ventana principal
+        self.geometry('300x130')
+        self.title('Login')
+        self.iconbitmap('Z_icon.ico')
+        self.resizable(0,0)
+        # configuración del grid
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=3)
+        # Creamos los componentes
+        self._crear_componentes()
+
+    # Definir el método crear_componentes
+    def _crear_componentes(self):
+        # usuario
+        usuario_etiqueta = ttk.Label(self, text='Usuario:')
+        usuario_etiqueta.grid(row=0, column=0, sticky=tk.E, padx=5, pady=5)
+        self.usuario_entrada = ttk.Entry(self)
+        self.usuario_entrada.grid(row=0, column=1, sticky=tk.W, padx=5, pady=5)
+
+        # password
+        password_etiqueta = ttk.Label(self, text='Password:')
+        password_etiqueta.grid(row=1, column=0, sticky=tk.E, padx=5, pady=5)
+        self.password_entrada = ttk.Entry(self, show='*')
+        self.password_entrada.grid(row=1, column=1, sticky=tk.W, padx=5, pady=5)
+
+        # boton Login
+        login_boton = ttk.Button(self, text='Login', command=self._login)
+        login_boton.grid(row=3, column=0, columnspan=2)
+
+
+    def _login(self):
+        messagebox.showinfo('Datos Login',
+            f'usuario: {self.usuario_entrada.get()}, Password: {self.password_entrada.get()}')
+
+# Ejecutar la ventana
+if __name__ == '__main__':
+    login_ventana = LoginVentana()
+    login_ventana.mainloop()
+
